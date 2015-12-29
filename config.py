b2923f070df48cf9937af76f9e6cdf51bf017ad5
WhatsCS <whatscs@32ndrangerbattalion.com>
2015-12-28 20:06:10 -0800

    place holder config file, no fucking clue how to use yet

---
+++ config.py
@@ -0,0 +1,9 @@
+# Create dummy key for now
+SECRET_KEY = '123456789'
+
+# Create in-memory databse
+DATABASE_URL = 'localhost'
+DATABASE_USERNAME = 'root'
+DATABASE_PASS = 'test123'
+
+# Flask-Security features
