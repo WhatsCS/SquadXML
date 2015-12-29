2cb49757eb01708598c320d8a1370de409795e97
WhatsCS <whatscs@32ndrangerbattalion.com>
2015-12-28 20:06:40 -0800

    I have no idea what I changed to be perfectly honest...

--- SquadXML.py
+++ SquadXML.py
@@ -14,52 +14,47 @@ be able to add in users and delete users.
 """
 from flask import Flask, url_for, redirect, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required, current_user
-import flask_admin
-from flask_admin.contrib import sqla
-from flask_admin import helpers as admin_helpers
+from flask_security import Security
+
+from wtforms import validators
+
+import flask_admin as admin
+from flask_admin import BaseView, expose
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
-
-# Define Roles
-roles_uesrs = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Interger(), db.ForeignKey('role.id'))
-)
 
 
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-
-    def __str__(self):
-        return self.name
+# db = SQLAlchemy(app)
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.String255))
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+# Customized admin interface
+class CustomView(ModelView):
+    list_template = 'list.html'
+    create_template = 'create.html'
+    edit_template = 'edit.html'
 
 
-    def __str__(self):
-        return self.email
-
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     player_id = db.Column(db.Integer)
+#     nick = db.Column(db.String(100), unique=True)
+#     name = db.Column(db.String(100))
+#     email = db.Column(db.String(100))
+#     icq = db.Column(db.Integer)
+#     remark = db.Column(db.String(128))
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template('index.html',
+                           title='Home')
 
+
+# Create admin with custom base template
+admin = admin.Admin(app, 'SquadXML: Layout', base_template='layout.html', template_mode='bootstrap3')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
