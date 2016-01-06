e03521dac1c9a8be110cecdcc5bea06259e81c1c
WhatsCS <whatscs@32ndrangerbattalion.com>
2016-01-05 16:55:47 -0800

    Added sqlite shit

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # Create dummy key for now
 SECRET_KEY = '123456789'
 
-# Create in-memory databse
-DATABASE_URL = 'localhost'
-DATABASE_USERNAME = 'root'
-DATABASE_PASS = 'test123'
+# Create in-memory database
+DATABASE_FILE = 'sample_db.sqlite'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
+SQLALCHEMY_ECHO = True
+
