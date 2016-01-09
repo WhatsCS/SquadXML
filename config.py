56e74434327fa0f55cd8030c4b50e3978a76e044
WhatsCS <whatscs@32ndrangerbattalion.com>
2016-01-09 14:44:20 -0800

    Added sqlalchemy option (to be removed probably). Also added xml configs.

--- config.py
+++ config.py
@@ -5,4 +5,15 @@ SECRET_KEY = '123456789'
 DATABASE_FILE = 'sample_db.sqlite'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
 SQLALCHEMY_ECHO = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+# Website URL
+WEB_URL = 'http://localhost:5000'
+
+# Squad Info
+SQUAD_NICK = '32ndRB'
+SQUAD_NAME = '32nd Ranger Battalion'
+SQUAD_EMAIL = 'church@32ndrangerbattalion.com'
+SQUAD_WEB = 'www.32ndrangerbattalion.com'
+#SQUAD_PICTURE =
+SQUAD_TITLE = '32nd Ranger Battalion'
